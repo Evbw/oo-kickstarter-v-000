fa1d0681d4c42a23e1bb6ec70489c97117de091e
Everett <everett.bowline@gmail.com>
2018-04-23 23:44:46 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,8 +5,9 @@ class Backer
       @backed_projects = []
     end
     
-    def self.back_project(project)
-      @backed_projects << project
+    def self.back_project(backer)
+      @backed_projects << backer
+      self.all.detect { | backer | backer.project == project } || Project.new(project)
     end
       
 end
