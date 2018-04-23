1aea16e1fd67e583fea1d09332e50357329f4567
Everett <everett.bowline@gmail.com>
2018-04-23 23:53:40 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
   attr_reader :backed_projects, :name
   
-    def initialize(name)
-      @backed_projects = []
-      @name = name
-    end
+   def initialize(name)
+     @backed_projects = []
+    @name = name
+   end
     
-    def back_project(project)
-      @backed_projects << project
-    end
+  def back_project(project)
+    @backed_projects << project
+  end
       
 end
