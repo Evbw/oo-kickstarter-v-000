707ddafb6ef24a509b28b613b612bb747a9fa0f9
Everett <everett.bowline@gmail.com>
2018-04-23 23:57:09 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -12,23 +12,3 @@ class Project
   end
   
 end
-
-#def artist_name=(name)
-#    self.artist = Artist.find_or_create_by_name(name) ||
-#    artist.add_song(self)
-#end
-
-#  def artist_name=(name)
-#    if (self.artist.nil?)
-#      self.artist = Artist.new(name)
-#    else
-#      self.artist.name = name
-#    end
-#  end
-
-#def self.find_or_create_by_name(name)
-#    self.all.detect { | artist | artist.name == name } || Artist.new(name)
-#    name.artist = self
-#    name
-#  end
-  
